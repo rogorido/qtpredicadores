94afa9a685a376cf4e0e11725cd59aa84ac21180
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-02-14 18:17:12 +0100

    Primera versión de sql para NuevoCapitulo

--- nuevocapitulo.cpp
+++ nuevocapitulo.cpp
@@ -3,6 +3,11 @@
 
 #include <QSqlQueryModel>
 #include <QCompleter>
+#include <QSqlQuery>
+
+#include <QDebug>
+#include <QMessageBox>
+#include <QInputDialog>
 
 /*
 #include <QSqlRecord>
@@ -22,6 +27,7 @@ NuevoCapitulo::NuevoCapitulo(QWidget *parent) :
 
     ui->cboTipoCapitulo->addItem("General");
     ui->cboTipoCapitulo->addItem("Provincial");
+    ui->cboTipoCapitulo->setCurrentIndex(-1);
 
     cargarCompleters();
 }
@@ -32,6 +38,73 @@ NuevoCapitulo::~NuevoCapitulo()
 }
 
 void NuevoCapitulo::aceptarCapitulo(){
+
+    if (ui->txtNombreGeneral->text().isEmpty()){
+        int ret = QMessageBox::warning(this, "No hay título general", "Introduzca por favor un título general");
+        return;
+    }
+
+    QString titulogeneral;
+    QString lugar;
+    QString maestrogeneral;
+    QString tipo;
+    QDate fechainicial;
+    QDate fechafinal;
+    QString tomo;
+    QString paginas;
+    QString notas;
+    QString asistentes;
+
+    titulogeneral = ui->txtNombreGeneral->text();
+
+    /* vamos extrayendo todos los datos */
+    (!ui->txtLugar->text().isEmpty()) ?
+                lugar = ui->txtLugar->text() : lugar = "";
+
+    (!ui->txtMaestroGeneral->text().isEmpty()) ?
+                maestrogeneral = ui->txtMaestroGeneral->text() : maestrogeneral = "";
+
+    (!ui->txtPaginas->text().isEmpty()) ?
+                paginas = ui->txtPaginas->text() : paginas = "";
+
+    (!ui->txtAsistentes->toPlainText().isEmpty()) ?
+                asistentes = ui->txtAsistentes->toPlainText() : asistentes = "";
+
+    (!ui->txtTomo->text().isEmpty()) ?
+                tomo = ui->txtTomo->text() : tomo = "";
+
+    (!ui->txtNotas->toPlainText().isEmpty()) ?
+                notas = ui->txtNotas->toPlainText() : notas ="";
+
+    (!ui->cboTipoCapitulo->currentText().isEmpty()) ?
+                tipo = ui->cboTipoCapitulo->currentText() : tipo = "";
+
+    fechainicial = ui->dtInicial->date();
+    fechafinal = ui->dtFinal->date();
+
+
+    QSqlQuery query;
+    //query.prepare("INSERT INTO capitulos(nombregeneral, lugar, fechainicio, fechafinal, tipo, maestrogeneral, asistentes, tomo, paginas, notas) "
+      //            "VALUES(:nombre, :lugar, :fechainicio, :fechafinal, :tipo, :maestro, :asistentes, :tomo, :paginas, :notas)" );
+    query.prepare("INSERT INTO capitulos(nombregeneral, lugar, tipo, maestrogeneral, asistentes, tomo, paginas, notas) "
+                  "VALUES(:nombre, :lugar, :tipo, :maestro, :asistentes, :tomo, :paginas, :notas)" );
+    query.bindValue(":nombre", titulogeneral);
+    query.bindValue(":lugar", 1);
+    query.bindValue(":tipo", tipo);
+    query.bindValue(":maestro", maestrogeneral);
+    query.bindValue(":asistentes", asistentes);
+    //query.bindValue(":fechainicio", fechainicial);
+    //query.bindValue(":fechafinal", fechafinal);
+    query.bindValue(":tomo", tomo);
+    query.bindValue(":paginas", paginas);
+    query.bindValue(":notas", notas);
+
+    if (query.exec()){
+        qDebug("Titulo: " + titulogeneral.toUtf8());
+        //qDebug("EXECUTED "+query.executedQuery().toAscii());
+        qDebug("EXECUTED "+query.executedQuery().toUtf8());
+        //qDebug("ultimo error: " + query.lastError().toUtf8);
+    }
 
 }
 
@@ -48,3 +121,4 @@ void NuevoCapitulo::cargarCompleters(){
     ui->txtLugar->setCompleter(lugar_completer);
 
 }
+
