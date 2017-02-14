333116c378784b0ddade6ef98db3cd9728c5b8b4
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-02-14 16:21:32 +0100

    Comienzo de funcionalidad en NuevoCapitulo

--- nuevocapitulo.cpp
+++ nuevocapitulo.cpp
@@ -1,14 +1,50 @@
 #include "nuevocapitulo.h"
 #include "ui_nuevocapitulo.h"
 
+#include <QSqlQueryModel>
+#include <QCompleter>
+
+/*
+#include <QSqlRecord>
+#include <QSqlQuery>
+*/
+
 NuevoCapitulo::NuevoCapitulo(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::NuevoCapitulo)
 {
     ui->setupUi(this);
+
+    connect(ui->btCancel, SIGNAL(clicked()), this, SLOT(close()));
+    connect(ui->btOK, SIGNAL(clicked()), this, SLOT(aceptarCapitulo()));
+
+    ui->dtFinal->calendarPopup();
+
+    ui->cboTipoCapitulo->addItem("General");
+    ui->cboTipoCapitulo->addItem("Provincial");
+
+    cargarCompleters();
 }
 
 NuevoCapitulo::~NuevoCapitulo()
 {
     delete ui;
+}
+
+void NuevoCapitulo::aceptarCapitulo(){
+
+}
+
+void NuevoCapitulo::cargarCompleters(){
+
+    lugar_query = new QSqlQueryModel(this);
+    lugar_query->setQuery("SELECT DISTINCT lugar FROM lugares WHERE lugar IS NOT NULL ORDER BY lugar");
+
+    lugar_completer = new QCompleter(this);
+    lugar_completer->setModel(lugar_query);
+    lugar_completer->setCompletionColumn(0);
+    lugar_completer->setCaseSensitivity(Qt::CaseInsensitive);
+
+    ui->txtLugar->setCompleter(lugar_completer);
+
 }
--- nuevocapitulo.h
+++ nuevocapitulo.h
@@ -3,6 +3,9 @@
 
 #include <QDialog>
 
+class QCompleter;
+class QSqlQueryModel;
+
 namespace Ui {
 class NuevoCapitulo;
 }
@@ -15,8 +18,26 @@ public:
     explicit NuevoCapitulo(QWidget *parent = 0);
     ~NuevoCapitulo();
 
+private slots:
+    void aceptarCapitulo();
+
 private:
     Ui::NuevoCapitulo *ui;
+
+    QCompleter *lugar_completer;
+
+    /* Esto sincermnete no se por que hay
+     * que ponerlo asi aqui. Lo logico seria ponerlo
+     * como variable local y luego pasarlo como referencia
+     * al Qcompleter; pero no funciona... ¿sera porque se pierde
+     * el modelo al salir?
+     */
+    QSqlQueryModel *lugar_query;
+
+    void cargarCompleters();
+
+
+
 };
 
 #endif // NUEVOCAPITULO_H
