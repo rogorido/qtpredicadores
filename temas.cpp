4260d1f55a97ee7d3db5b8c7b11be373dfc50f27
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-02-15 09:59:57 +0100

    Nueva clase Temas

---
+++ temas.cpp
@@ -0,0 +1,6 @@
+#include "temas.h"
+
+Temas::Temas()
+{
+
+}
---
+++ temas.h
@@ -0,0 +1,11 @@
+#ifndef TEMAS_H
+#define TEMAS_H
+
+
+class Temas : public QSqlTableModel
+{
+public:
+    Temas();
+};
+
+#endif // TEMAS_H
