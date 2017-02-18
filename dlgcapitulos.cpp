10bd09ffc29d0c575a74b0aa34377f4552daa8bf
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-02-18 17:50:07 +0100

    Form Capitulos mejorado

--- dlgcapitulos.cpp
+++ dlgcapitulos.cpp
@@ -2,6 +2,7 @@
 #include "ui_dlgcapitulos.h"
 
 #include <QSqlTableModel>
+#include <QDebug>
 
 DlgCapitulos::DlgCapitulos(QWidget *parent) :
     QDialog(parent),
@@ -14,6 +15,13 @@ DlgCapitulos::DlgCapitulos(QWidget *parent) :
     m_capitulos->select();
 
     ui->twCapitulos->setModel(m_capitulos);
+
+    // esto es para hacer el loop de un array de ints...
+    // la finalidad: ocultar algunas columnas
+    std::vector<int> v = {0, 2, 6, 7, 8, 9, 10, 11, 12};
+    for(int n : v) {
+        ui->twCapitulos->hideColumn(n);
+        }
 }
 
 DlgCapitulos::~DlgCapitulos()
