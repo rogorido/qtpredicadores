d2e13a9339bc3ebad14465e69ef7ec4e516f7b33
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-02-25 19:27:39 +0100

    Nueva clase ProvinciasModel

---
+++ models/provinciasmodel.cpp
@@ -0,0 +1,46 @@
+#include "provinciasmodel.h"
+
+#include <QSqlQuery>
+
+#include "objs/provincia.h"
+
+ProvinciasModel *ProvinciasModel::pInstance = 0;
+
+ProvinciasModel::ProvinciasModel() :
+    QSqlTableModel()
+{
+    this->setTable("provincias");
+
+}
+
+ProvinciasModel *ProvinciasModel::InstanceModel(){
+    if (pInstance == 0){
+        pInstance = new ProvinciasModel();
+        atexit(&DestroyMe);
+    }
+
+    return pInstance;
+}
+
+void ProvinciasModel::DestroyMe(){
+    if (pInstance != NULL) delete pInstance;
+}
+
+void ProvinciasModel::AnadirProvincia(const Provincia *provincia){
+    QSqlQuery query;
+
+    QString nombre = provincia->getNombre();
+    QString otros_nombres = provincia->getOtrosNombres();
+    QString erigida = provincia->getErigida();
+    QString notas = provincia->getNotas();
+
+    query.prepare("INSERT INTO provincias(nombre, otrosnombres, erigida, notas) "
+                  "VALUES(:nombre, :otrosnombres, :erigida, :notas)");
+    query.bindValue(":nombre", nombre);
+    query.bindValue(":otrosnombres", otros_nombres);
+    query.bindValue(":erigida", erigida);
+    query.bindValue(":notas", notas);
+    query.exec();
+
+    this->select();
+}
---
+++ models/provinciasmodel.h
@@ -0,0 +1,28 @@
+#ifndef PROVINCIASMODEL_H
+#define PROVINCIASMODEL_H
+
+#include <QSqlTableModel>
+
+class Provincia;
+
+class ProvinciasModel : public QSqlTableModel
+{
+    Q_OBJECT
+
+public:
+    static ProvinciasModel *InstanceModel();
+
+    void AnadirProvincia(const Provincia *prov);
+
+protected:
+    ProvinciasModel();
+    ProvinciasModel(const ProvinciasModel &);
+    ProvinciasModel &operator =(const ProvinciasModel &);
+
+private:
+    static ProvinciasModel *pInstance;
+    static void DestroyMe();
+
+};
+
+#endif // PROVINCIASMODEL_H
