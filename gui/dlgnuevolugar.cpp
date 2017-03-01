bf48f28da117b620b97fe0ac6100a99d015e4bf5
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-03-01 18:43:22 +0100

    Completando funcionalidad a NuevoLugar

--- gui/dlgnuevolugar.cpp
+++ gui/dlgnuevolugar.cpp
@@ -16,8 +16,11 @@ dlgNuevoLugar::dlgNuevoLugar(QWidget *parent) :
 
     m_lugares = LugaresModel::InstanceModel();
 
+    dlgdetalles = new dlgDetalles(otrosnombres, this);
+
     connect(ui->btCancelar, SIGNAL(clicked(bool)), this, SLOT(close()));
     connect(ui->btOK, SIGNAL(clicked(bool)), this, SLOT(aceptar()));
+    connect(ui->btOtrosNombres, SIGNAL(clicked(bool)), dlgdetalles, SLOT(show()));
 
     cargarModelos();
 }
@@ -35,6 +38,8 @@ void dlgNuevoLugar::aceptar(){
 
     lugar = ui->txtLugar->text();
     pais = ui->txtPais->text();
+
+    otrosnombres->actualizarPrevioIntroducir();
 
     if (otrosnombres->getSize() > 0){
         // entiendo q solo puede haber un elemento en la Qlist...
@@ -55,7 +60,5 @@ void dlgNuevoLugar::cargarModelos(){
 
     m_paises->setQuery("SELECT DISTINCT pais FROM lugares ORDER BY pais");
     m_paises_completer->setModel(m_paises);
-
-    dlgdetalles = new dlgDetalles(otrosnombres, this);
 
 }
