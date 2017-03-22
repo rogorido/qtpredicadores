6664fce1bb47880802364bd87cc943cb57ac332b
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-03-22 23:11:17 +0100

    Funcionalidad en form dlgTemas

--- gui/dlgtemas.cpp
+++ gui/dlgtemas.cpp
@@ -1,14 +1,88 @@
 #include "dlgtemas.h"
 #include "ui_dlgtemas.h"
 
+#include <QInputDialog>
+#include <QSqlRecord>
+#include <QSqlQuery>
+#include <QTableWidget>
+
+#include "models/temasmodel.h"
+
 dlgTemas::dlgTemas(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::dlgTemas)
 {
     ui->setupUi(this);
+
+    connect(ui->btCancelar, SIGNAL(clicked()), this, SLOT(close()));
+    connect(ui->btOK, SIGNAL(clicked()), this, SLOT(aceptarTemas()));
+
+    m_temas = TemasModel::InstanceModel();
+    rellenarCombos();
 }
 
 dlgTemas::~dlgTemas()
 {
     delete ui;
+}
+
+void dlgTemas::rellenarCombos(){
+// TODO: modificar
+    ui->cboTemas->setModel(m_temas);
+    ui->cboTemas->setCurrentIndex(-1);
+    ui->cboTemas->setModelColumn(1);
+}
+
+void dlgTemas::on_btNuevoTema_clicked(){
+
+    QString tema;
+
+    tema = QInputDialog::getText(this, "Introduzca nuevo tema", "Nueva tema");
+
+    if (!tema.isEmpty())
+        m_temas->AnadirTema(tema);
+
+}
+
+void dlgTemas::on_btAnadirTema_clicked(){
+    /* sacamos el record del combobox */
+
+    QSqlRecord record = m_temas->record(ui->cboTemas->currentIndex());
+    int id = record.value(0).toInt();
+    QString tema = ui->cboTemas->currentText();
+
+    /* creamos un nuevo struct y lo añadimos a la lista */
+    elementopareado nuevo;
+    nuevo.id = id;
+    nuevo.elemento = tema;
+    temas_lista.append(nuevo);
+
+    /* añadimos un elem a la tabla */
+    QTableWidgetItem *item = new QTableWidgetItem(tema);
+    //this will give the present number of rows available.
+    int insertRow = ui->twTemas->rowCount();
+    //insert the row at the bottom of the table widget - using.
+    ui->twTemas->insertRow(insertRow);
+
+    ui->twTemas->setItem(insertRow, 0, item);
+}
+
+void dlgTemas::on_btQuitarTema_clicked(){
+
+    QString valor = ui->twTemas->currentIndex().data().toString();
+
+    int row = ui->twTemas->currentRow();
+    ui->twTemas->removeRow(row);
+
+    for (int i = 0; i < temas_lista.size(); ++i) {
+      if(temas_lista.at(i).elemento == valor){
+        temas_lista.removeAt(i);
+        break;
+      }
+     }
+
+}
+
+void dlgTemas::aceptarTemas(){
+    emit(temasSeleccionadosSignal(temas_lista));
 }
--- gui/dlgtemas.h
+++ gui/dlgtemas.h
@@ -3,6 +3,8 @@
 
 #include <QDialog>
 
+class TemasModel;
+
 namespace Ui {
 class dlgTemas;
 }
@@ -15,8 +17,35 @@ public:
     explicit dlgTemas(QWidget *parent = 0);
     ~dlgTemas();
 
+private slots:
+
+    void on_btNuevoTema_clicked();
+    void on_btAnadirTema_clicked();
+    void on_btQuitarTema_clicked();
+
+    void aceptarTemas();
+
 private:
     Ui::dlgTemas *ui;
+
+    TemasModel *m_temas;
+
+    struct elementopareado{
+      int id;
+      QString elemento;
+    };
+
+    QList<elementopareado> temas_lista;
+
+    void rellenarCombos();
+
+signals:
+    /*
+     * curioso: esto tiene que ir después del bloque private:
+     * pq si no da un error por el elementopareado
+     */
+    void temasSeleccionadosSignal(QList<elementopareado> temas);
+
 };
 
 #endif // DLGTEMAS_H
