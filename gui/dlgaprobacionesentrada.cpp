5c7249dcbcc29990143ad89e7b13b19e49ec3219
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-04-29 13:19:41 +0200

    Signals, etc. en la nueva clase dlgAprobacionesEntrada, formulario para meter aprobaciones

--- gui/dlgaprobacionesentrada.cpp
+++ gui/dlgaprobacionesentrada.cpp
@@ -1,14 +1,55 @@
 #include "dlgaprobacionesentrada.h"
 #include "ui_dlgaprobacionesentrada.h"
 
+#include "gui/dlgseleccionargeneral.h"
+
 dlgAprobacionesEntrada::dlgAprobacionesEntrada(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::dlgAprobacionesEntrada)
 {
     ui->setupUi(this);
+
+    aprobacion_activa = new Aprobacion();
+
+    connect(ui->btCancelar, SIGNAL(clicked(bool)), this, SLOT(close()));
+    connect(ui->btPersona, SIGNAL(clicked(bool)), this, SLOT(anadirPersona()));
+    connect(ui->btProvincia, SIGNAL(clicked(bool)), this, SLOT(anadirProvincia()));
+    connect(ui->btAnadirAprobacion, SIGNAL(clicked(bool)), this, SLOT(anadirAprobacion()));
+    connect(ui->btOK, SIGNAL(clicked(bool)), this, SLOT(aceptarAprobaciones()));
+
 }
 
 dlgAprobacionesEntrada::~dlgAprobacionesEntrada()
 {
     delete ui;
+}
+
+void dlgAprobacionesEntrada::anadirAprobacion()
+{
+
+}
+
+void dlgAprobacionesEntrada::aceptarAprobaciones()
+{
+
+}
+
+void dlgAprobacionesEntrada::anadirPersona()
+{
+
+}
+
+void dlgAprobacionesEntrada::anadirProvincia()
+{
+
+}
+
+void dlgAprobacionesEntrada::actualizarPersona(Persona persona)
+{
+
+}
+
+void dlgAprobacionesEntrada::actualizarProvincia(Provincia provincia)
+{
+
 }
--- gui/dlgaprobacionesentrada.h
+++ gui/dlgaprobacionesentrada.h
@@ -3,6 +3,12 @@
 
 #include <QDialog>
 
+#include "objs/aprobacion.h"
+#include "objs/persona.h"
+#include "objs/provincia.h"
+
+class dlgSeleccionarGeneral;
+
 namespace Ui {
 class dlgAprobacionesEntrada;
 }
@@ -15,8 +21,24 @@ public:
     explicit dlgAprobacionesEntrada(QWidget *parent = 0);
     ~dlgAprobacionesEntrada();
 
+signals:
+
+    void aceptarDatos(QList<Aprobacion> listaaprobaciones);
+
+private slots:
+    void anadirAprobacion();
+    void aceptarAprobaciones();
+    void anadirPersona();
+    void anadirProvincia();
+
+    void actualizarPersona(Persona persona);
+    void actualizarProvincia(Provincia provincia);
+
 private:
     Ui::dlgAprobacionesEntrada *ui;
+    dlgSeleccionarGeneral *dlgseleccionar;
+
+    Aprobacion *aprobacion_activa;
 };
 
 #endif // DLGAPROBACIONESENTRADA_H
