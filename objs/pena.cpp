e757c805e56e16ae2c0c5bd7042dc84d4d399e9c
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-04-30 17:52:08 +0200

    Código de la nueva clase Pena

--- objs/pena.cpp
+++ objs/pena.cpp
@@ -1,6 +1,12 @@
 #include "pena.h"
 
-Pena::Pena()
-{
+Pena::Pena() { }
 
-}
+void Pena::setPenasTipos(QStringList p) { penas_tipos = p; }
+void Pena::setPenaTexto(QString p) { pena_texto = p; }
+void Pena::setDuracion(QString d) { duracion = d; }
+void Pena::setRestriccion(QString r) { restriccion = r; }
+void Pena::setAbsolucion(QString a) { absolucion = a; }
+void Pena::setPenados(QStringList p) { penados = p; }
+void Pena::setMotivo(QString m) { motivo = m; }
+void Pena::setSeguridad(int s) { seguridad = s; }
