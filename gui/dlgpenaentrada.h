0767aaad16b95af5d01b42f350140929913f6167
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-04-30 18:31:26 +0200

    Header del nuevo formulario para meter penas

--- gui/dlgpenaentrada.h
+++ gui/dlgpenaentrada.h
@@ -3,6 +3,11 @@
 
 #include <QDialog>
 
+#include "objs/pena.h"
+
+class QSqlQueryModel;
+class QCompleter;
+
 namespace Ui {
 class dlgPenaEntrada;
 }
@@ -15,8 +20,30 @@ public:
     explicit dlgPenaEntrada(QWidget *parent = 0);
     ~dlgPenaEntrada();
 
+private slots:
+
+    void aceptar();
+    void anadirPenasTipos();
+    void quitarPenasTipos();
+    void anadirPenados();
+    void quitarPenados();
+
 private:
     Ui::dlgPenaEntrada *ui;
+
+    QStringList penastipos;
+    QStringList penados;
+
+    QSqlQueryModel *tipos_model;
+    QCompleter *tipos_completer;
+    QSqlQueryModel *penados_model;
+    QCompleter *penados_completer;
+    QSqlQueryModel *penatexto_model;
+    QCompleter *penatexto_completer;
+
+    Pena pena;
+
+    void cargarModelos();
 };
 
 #endif // DLGPENAENTRADA_H
