4465bd25584d9487ad5737563c744982fb1395fe
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-05-02 16:45:49 +0200

    Funcionalidad de la nueva clase Afiliacion

--- objs/afiliacion.cpp
+++ objs/afiliacion.cpp
@@ -1,6 +1,8 @@
 #include "afiliacion.h"
 
-Afiliacion::Afiliacion()
-{
+Afiliacion::Afiliacion() { }
 
-}
+void Afiliacion::setPersona(Persona p) { persona = p; }
+void Afiliacion::setCasa(Casa c) { casa = c; }
+void Afiliacion::setProvincia(Provincia p) { provincia = p; }
+void Afiliacion::setExtras(ExtraInfos e) { extras = e; }
