0390f9d3a98cbd903190326cdf10f75f3b1864fb
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-05-02 17:06:48 +0200

    Interface del nuevo form para meter afiliaciones

--- gui/dlgafiliacionentrada.h
+++ gui/dlgafiliacionentrada.h
@@ -3,6 +3,13 @@
 
 #include <QDialog>
 
+#include <objs/afiliacion.h>
+#include "objs/persona.h"
+#include "objs/provincia.h"
+#include "objs/variados.h"
+
+#include "models/afiliacionestablemodel.h"
+
 namespace Ui {
 class dlgAfiliacionEntrada;
 }
@@ -15,8 +22,31 @@ public:
     explicit dlgAfiliacionEntrada(QWidget *parent = 0);
     ~dlgAfiliacionEntrada();
 
+signals:
+
+    void aceptarDatos(QList<Afiliacion*> listaafiliciones);
+
+private slots:
+    void anadirAfiliacion();
+    void quitarAfiliacion();
+    void aceptarAfiliaciones();
+    void anadirPersona();
+    void anadirProvincia();
+
+    void actualizarPersona(Persona persona);
+    void actualizarProvincia(Provincia provincia);
+
 private:
     Ui::dlgAfiliacionEntrada *ui;
+    AfiliacionesTableModel *afiliaciones_model;
+
+    /*
+     * lo hago pointer pq luego necesito borrarla
+     * para crear una nueva... pero esto sinceramente no sé
+     * si está muy bien así. Al hacerlo pointer esto tb
+     * me cambia internamente lo de AprobacionesTableModel
+     */
+    Afiliacion *aprobacion_activa;
 };
 
 #endif // DLGAFILIACIONENTRADA_H
