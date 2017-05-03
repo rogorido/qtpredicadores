f5a6cf687ab7404dce3daf19ab3114e98951f0c3
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-05-03 18:37:23 +0200

    Interface de la nueva clase QJsonTreeItem

--- objs/qjsontreeitem.h
+++ objs/qjsontreeitem.h
@@ -6,6 +6,37 @@ class QJsonTreeItem
 {
 public:
     QJsonTreeItem();
+    ~QJsonTreeItem();
+
+    QJsonTreeItem *child(int row);
+    QJsonTreeItem *parent();
+
+    void setKey(const QString& key);
+    void setValue(const QString& value);
+    void setType(const QJsonValue::Type& type);
+
+    void appendChild(QJsonTreeItem * item);
+
+    // getters
+    QString key() const;
+    QString value() const;
+    QJsonValue::Type type() const;
+
+    int childCount() const;
+    int row() const;
+
+    static QJsonTreeItem* load(const QJsonValue& value, QJsonTreeItem * parent = 0);
+
+protected:
+
+private:
+
+    QString mKey;
+    QString mValue;
+    QJsonValue::Type mType;
+    QList<QJsonTreeItem*> mChilds;
+    QJsonTreeItem * mParent;
+
 };
 
 #endif // QJSONTREEITEM_H
