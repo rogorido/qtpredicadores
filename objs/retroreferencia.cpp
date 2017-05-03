78679f71deef0f11358028474e1a7a319ade7936
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-05-03 17:30:54 +0200

    Nueva clase RetroReferencia

---
+++ objs/retroreferencia.cpp
@@ -0,0 +1,6 @@
+#include "retroreferencia.h"
+
+RetroReferencia::RetroReferencia()
+{
+
+}
---
+++ objs/retroreferencia.h
@@ -0,0 +1,11 @@
+#ifndef RETROREFERENCIA_H
+#define RETROREFERENCIA_H
+
+
+class RetroReferencia
+{
+public:
+    RetroReferencia();
+};
+
+#endif // RETROREFERENCIA_H
