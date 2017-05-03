98697c5d4fcb9ee05ffafaf9785008e0ef90aae1
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-05-03 17:40:23 +0200

    Funcionalidad json en la nueva clase RetroReferencia

--- objs/retroreferencia.cpp
+++ objs/retroreferencia.cpp
@@ -12,3 +12,14 @@ void RetroReferencia::setTipo(QString t)
 void RetroReferencia::setReferencia(QString r) { referencia = r; }
 void RetroReferencia::setTextoReal(QString t) { texto_real = t; }
 
+QJsonObject RetroReferencia::getRetroJson()
+{
+    QJsonObject json;
+
+    json.insert("tipo_retro", tipo);
+    json.insert("referencia", referencia);
+    json.insert("textoreal_retro", texto_real);
+
+    return json;
+}
+
