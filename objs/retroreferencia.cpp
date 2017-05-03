7d1153e286cf0b5e262c5b23abc379f7cb2cd913
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-05-03 17:38:27 +0200

    Funcionalidad básica de la nueva clase RetroReferencia

--- objs/retroreferencia.cpp
+++ objs/retroreferencia.cpp
@@ -1,6 +1,14 @@
 #include "retroreferencia.h"
 
 RetroReferencia::RetroReferencia()
-{
+{ lleno = false; }
 
+void RetroReferencia::setTipo(QString t)
+{
+    tipo = t;
+    lleno = true;
 }
+
+void RetroReferencia::setReferencia(QString r) { referencia = r; }
+void RetroReferencia::setTextoReal(QString t) { texto_real = t; }
+
--- objs/retroreferencia.h
+++ objs/retroreferencia.h
@@ -1,11 +1,38 @@
 #ifndef RETROREFERENCIA_H
 #define RETROREFERENCIA_H
 
+#include <QJsonObject>
+
+#include "objs/variados.h"
 
 class RetroReferencia
 {
 public:
     RetroReferencia();
+
+    // setters
+
+    void setTipo(QString t);
+    void setReferencia(QString r);
+    void setTextoReal(QString t);
+
+    // getters
+    QString getTipo() const {return tipo;}
+    QString getReferencia() const {return referencia;} // es un valor/resumen que pongo yo
+    QString getTextoTeal() const {return texto_real;}
+    bool estaLleno() const {return lleno;}
+
+    // devolver un QJsonObject
+    QJsonObject getRetroJson();
+
+private:
+
+    QString tipo;
+    QString referencia; // es un valor/resumen que pongo yo
+    QString texto_real;
+    bool lleno = false;
+
+
 };
 
 #endif // RETROREFERENCIA_H
