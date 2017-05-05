267b8b28124daaf7f5e5fc56567bab4021fe444c
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-05-05 23:16:42 +0200

    Interface de la clase Sufragio

--- objs/sufragio.h
+++ objs/sufragio.h
@@ -1,11 +1,35 @@
 #ifndef SUFRAGIO_H
 #define SUFRAGIO_H
 
+#include <QString>
+#include <QStringList>
+#include <QJsonObject>
 
 class Sufragio
 {
 public:
     Sufragio();
+
+    // setters
+    void setTipo(QString t);
+    void setMisas(int m);
+    void setMotivo(QString m);
+    void setDestinatarios(QStringList d);
+
+    // getters
+    QString getTipo() const {return tipo;}
+    int getMisas() const {return misas;}
+    QString getMotivo() const {return motivo;}
+    QStringList getDestinatarios() const {return destinatarios;}
+
+    QJsonObject getSufragio();
+
+private:
+
+    QString tipo;
+    int misas;
+    QString motivo;
+    QStringList destinatarios;
 };
 
 #endif // SUFRAGIO_H
