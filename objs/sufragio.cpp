4712bda6b3eb2d0aecc7671dc9a38afca4ddb76b
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-05-05 23:10:15 +0200

    Añadida clase Sufragio

---
+++ objs/sufragio.cpp
@@ -0,0 +1,6 @@
+#include "sufragio.h"
+
+Sufragio::Sufragio()
+{
+
+}
---
+++ objs/sufragio.h
@@ -0,0 +1,11 @@
+#ifndef SUFRAGIO_H
+#define SUFRAGIO_H
+
+
+class Sufragio
+{
+public:
+    Sufragio();
+};
+
+#endif // SUFRAGIO_H
