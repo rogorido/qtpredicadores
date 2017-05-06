916ae161bd1de3333ed888c65c21f1c420887bc7
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-05-06 12:27:35 +0200

    Uso del widget en un formulario: funciona!

--- widgets/widgettemas.cpp
+++ widgets/widgettemas.cpp
@@ -12,19 +12,33 @@ WidgetTemas::WidgetTemas(QWidget *parent) :
     ui->setupUi(this);
 
     m_temas = TemasModel::InstanceModel();
+    m_temas->sort(1, Qt::AscendingOrder);
 
     temas_noseleccionados_proxy = new QSortFilterProxyModel(this);
     temas_noseleccionados_proxy->setSourceModel(m_temas);
-    temas_noseleccionados_proxy->setFilterFixedString("0");
-    temas_noseleccionados_proxy->setFilterKeyColumn(2);
+    temas_noseleccionados_proxy->setFilterFixedString("f");
+    temas_noseleccionados_proxy->setFilterKeyColumn(3);
 
     temas_seleccionados_proxy = new QSortFilterProxyModel(this);
     temas_seleccionados_proxy->setSourceModel(m_temas);
-    temas_seleccionados_proxy->setFilterFixedString("1");
-    temas_seleccionados_proxy->setFilterKeyColumn(2);
+    temas_seleccionados_proxy->setFilterFixedString("t");
+    temas_seleccionados_proxy->setFilterKeyColumn(3);
 
     ui->twNoSeleccionado->setModel(temas_noseleccionados_proxy);
     ui->twSeleccionado->setModel(temas_seleccionados_proxy);
+
+    ui->twNoSeleccionado->hideColumn(0);
+    ui->twNoSeleccionado->hideColumn(2);
+    ui->twNoSeleccionado->hideColumn(3);
+
+    ui->twSeleccionado->hideColumn(0);
+    ui->twSeleccionado->hideColumn(2);
+    ui->twSeleccionado->hideColumn(3);
+
+    ui->twNoSeleccionado->resizeColumnsToContents();
+    ui->twNoSeleccionado->resizeRowsToContents();
+    ui->twSeleccionado->resizeColumnsToContents();
+    ui->twSeleccionado->resizeRowsToContents();
 
 }
 
