b98550cdf43dbd5b55d31535a73b5b61f0b98fa5
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-05-07 19:27:29 +0200

    Interface de la clas Notas

--- objs/notas.h
+++ objs/notas.h
@@ -1,11 +1,25 @@
 #ifndef NOTAS_H
 #define NOTAS_H
 
+#include <QString>
 
 class Notas
 {
 public:
     Notas();
+
+    // setters
+    void setVolverMirar(bool v);
+    void setNota(QString n);
+
+    // getters
+    bool getVolverMirar() const {return volveramirar;}
+    QString getNota() const {return nota;}
+
+private:
+    bool volveramirar = false;
+    QString nota;
+
 };
 
 #endif // NOTAS_H
