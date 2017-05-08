6f4615b3b99a9763e44ec8ccbb9480a4c0221db5
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-05-08 12:31:26 +0200

    Más interface de la nueva clase Declaracion

--- objs/declaracion.h
+++ objs/declaracion.h
@@ -7,6 +7,7 @@
 #include "objs/infraccion.h"
 #include "objs/pena.h"
 #include "objs/notas.h"
+#include "objs/persona.h"
 
 class Declaracion
 {
@@ -16,7 +17,8 @@ public:
     void setTipo(QString t);
     void setInfraccion(Infraccion i);
     void setPena(Pena p);
-    void setNota(Nota n);
+    void setPersona(Persona p);
+    void setNota(Notas n);
     void setExtraInfos(ExtraInfos e);
     void setExtraJson(QJsonObject e);
 
@@ -24,6 +26,7 @@ public:
     QString getTipo() const {return tipo;}
     Infraccion getInfraccion() const {return infraccion;}
     Pena getPena() const {return pena;}
+    Persona getPersona() const {return persona;}
     Notas getNota() const {return nota;}
     ExtraInfos getExtras() const {return extras;}
     QJsonObject getExtrajson() const {return extrajson;}
@@ -34,6 +37,7 @@ private:
     QString tipo;
     Infraccion infraccion;
     Pena pena;
+    Persona persona; // tvz tendría que ser Qlist... o QList<int>
     Notas nota;
     ExtraInfos extras;
     QJsonObject extrajson; // esto casi debería ser una QList<QJsonObject>...
