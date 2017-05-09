7989dbc3d506a69f22fc15cba195de0b171ea56f
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-05-09 23:24:50 +0200

    Funcionalidad básica de la nueva clase Declaracion

--- objs/declaracion.cpp
+++ objs/declaracion.cpp
@@ -4,3 +4,16 @@ Declaracion::Declaracion()
 {
 
 }
+
+void Declaracion::setTipo(QString t) { tipo = t; }
+void Declaracion::setInfraccion(Infraccion i) { infraccion = i; }
+void Declaracion::setPena(Pena p) { pena = p; }
+void Declaracion::setPersona(Persona p) { persona = p; }
+void Declaracion::setNota(Notas n) { nota = n; }
+void Declaracion::setExtraInfos(ExtraInfos e) { extras = e; }
+void Declaracion::setExtraJson(QJsonObject e) { extrajson = e; }
+
+QJsonObject Declaracion::getDeclaracionJson(){
+
+}
+
