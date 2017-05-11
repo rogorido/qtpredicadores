73ec16d4a6e7a83303c8cf618a59df105fe9a351
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-05-11 23:28:45 +0200

    Inicio de la interfaz de la nueva clase Diocesis

--- objs/diocesis.h
+++ objs/diocesis.h
@@ -1,11 +1,26 @@
 #ifndef DIOCESIS_H
 #define DIOCESIS_H
 
+#include <QString>
+#include <QJsonObject>
 
 class Diocesis
 {
 public:
     Diocesis();
+
+private:
+
+    int id;
+    QString nombre;
+    QString nombre_latin;
+    bool archidiocesis;
+    int sufraganeade;
+    int lugar;
+    bool existe_hoy;
+    QJsonObject otros_datos;
+
+
 };
 
 #endif // DIOCESIS_H
