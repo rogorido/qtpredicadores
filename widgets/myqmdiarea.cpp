db0b6c7cc7c2a287e01bc0d07c05671e484971c3
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-09-04 17:46:49 +0200

    QMdiArea como singleton

--- widgets/myqmdiarea.cpp
+++ widgets/myqmdiarea.cpp
@@ -1,6 +1,21 @@
 #include "myqmdiarea.h"
 
+MyQmdiArea *MyQmdiArea::pInstance = 0;
+
 MyQmdiArea::MyQmdiArea(QWidget *parent) : QMdiArea(parent)
 {
 
+}
+
+MyQmdiArea *MyQmdiArea::Instance(QWidget *parent){
+    if (pInstance == 0){
+        pInstance = new MyQmdiArea(parent);
+        atexit(&DestroyMe);
+    }
+
+    return pInstance;
+}
+
+void MyQmdiArea::DestroyMe(){
+    if (pInstance != NULL) delete pInstance;
 }
--- widgets/myqmdiarea.h
+++ widgets/myqmdiarea.h
@@ -10,7 +10,17 @@ class MyQmdiArea : public QMdiArea
     Q_OBJECT
 
 public:
+    static MyQmdiArea *Instance(QWidget *parent = 0);
+
+protected:
     MyQmdiArea(QWidget *parent = 0);
+    MyQmdiArea(const MyQmdiArea &);
+    MyQmdiArea &operator =(const MyQmdiArea &);
+
+private:
+
+    static MyQmdiArea *pInstance;
+    static void DestroyMe();
 };
 
 #endif // MYQMDIAREA_H
