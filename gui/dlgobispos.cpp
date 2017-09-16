7ac1a1cca20fb33709df6c329366ea34e7fcb294
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-09-16 20:50:42 +0200

    Filtrar por volveramirar

--- gui/dlgobispos.cpp
+++ gui/dlgobispos.cpp
@@ -3,22 +3,59 @@
 
 #include <QSqlQueryModel>
 
-const QString sqlgeneral = "SELECT * from bishops.bishops";
+const QString sqlgeneral = "SELECT * from vistas.obispos_general";
+const QString sqlvolvermirar = "SELECT bishop_id FROM bishops_details "
+                               "WHERE (details->'meta_info'->>'volver_a_mirar')::boolean = TRUE";
 
 dlgObispos::dlgObispos(QWidget *parent) :
-    QDialog(parent),
+    QWidget(parent),
     ui(new Ui::dlgObispos)
 {
     ui->setupUi(this);
 
     obispos_model = new QSqlQueryModel(this);
-    obispos_model->setQuery(sqlgeneral);
-
-    ui->twObispos->setModel(obispos_model);
-
+    sqlactivo = sqlgeneral;
+    cargarModelos();
 }
 
 dlgObispos::~dlgObispos()
 {
     delete ui;
+}
+
+void dlgObispos::cargarModelos()
+{
+    obispos_model->setQuery(sqlactivo);
+
+    ui->twObispos->setModel(obispos_model);
+
+    ui->twObispos->hideColumn(0);
+    ui->twObispos->setAlternatingRowColors(true);
+    //ui->twResoluciones->setColumnWidth(1, 80);
+    //ui->twResoluciones->resizeColumnsToContents();
+    //ui->twResoluciones->resizeRowsToContents();
+    ui->twObispos->horizontalHeader()->setStretchLastSection(true);
+    ui->twObispos->setSortingEnabled(true);
+    ui->twObispos->setSelectionBehavior(QAbstractItemView::SelectRows);
+    ui->twObispos->setSelectionMode(QAbstractItemView::SingleSelection);
+
+    // escogemos la primera línea del modelo...
+    QModelIndex index = obispos_model->index(0,0);
+    if (index.isValid()) {
+        ui->twObispos->setCurrentIndex(index);
+    }
+}
+
+void dlgObispos::on_ckVolverAMirar_toggled(bool checked)
+{
+    if (checked){
+        volveramirar = true;
+        sqlactivo = sqlgeneral + " WHERE vistas.obispos_general.bishop_id IN (" + sqlvolvermirar + ")";
+    }
+    else {
+        volveramirar = false;
+        sqlactivo = sqlgeneral;
+    }
+
+    obispos_model->setQuery(sqlactivo);
 }
--- gui/dlgobispos.h
+++ gui/dlgobispos.h
@@ -1,7 +1,7 @@
 #ifndef DLGOBISPOS_H
 #define DLGOBISPOS_H
 
-#include <QDialog>
+#include <QWidget>
 
 class QSqlQueryModel;
 
@@ -9,7 +9,7 @@ namespace Ui {
 class dlgObispos;
 }
 
-class dlgObispos : public QDialog
+class dlgObispos : public QWidget
 {
     Q_OBJECT
 
@@ -17,10 +17,26 @@ public:
     explicit dlgObispos(QWidget *parent = 0);
     ~dlgObispos();
 
+private slots:
+    void on_ckVolverAMirar_toggled(bool checked);
+
 private:
     Ui::dlgObispos *ui;
 
     QSqlQueryModel *obispos_model;
+
+    /*
+     * guardamos el SQL que está activo
+     */
+    QString sqlactivo;
+
+    /*
+     * esta variable guarda si estamos viendo los que
+     * hay que volver a mirar
+     */
+    bool volveramirar = false;
+
+    void cargarModelos();
 };
 
 #endif // DLGOBISPOS_H
