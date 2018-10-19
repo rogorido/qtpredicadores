b812eb97ee58484da58f3da6197c49c4c01c99a5
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2018-10-20 00:10:05 +0200

    Añadido dir-locals...

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((c++-mode . ((projectile-project-compilation-dir . "build/")
+              (projectile-project-compilation-cmd . "make")
+              (projectile-project-run-cmd . "~/programacion/qt/qtPredicadores/build/qtPredicadores"))))
